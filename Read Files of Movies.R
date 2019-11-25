ef92c7981c5b140681eeb70741bbffbb6ba02e7b
jmcurbelo <jmoyacurbelo@gmail.com>
2019-11-25 16:37:15 -0600

    Lectura de los archivos

---
+++ Read Files of Movies.R
@@ -0,0 +1,4 @@
+links <- read.csv("./Data/links.csv", header = TRUE)
+movies <- read.csv("./Data/movies.csv", header = TRUE)
+ratings <- read.csv("./Data/ratings.csv", header = TRUE)
+tags <- read.csv("./Data/tags.csv", header = TRUE)
