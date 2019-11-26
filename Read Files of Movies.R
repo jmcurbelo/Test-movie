3177fbac4798616635fcf852fc1abe9520bee698
jmcurbelo <jmoyacurbelo@gmail.com>
2019-11-26 13:36:03 -0600

    Cambio a función la lectura de los archivos para manejarlos más fácil

--- Read Files of Movies.R
+++ Read Files of Movies.R
@@ -1,4 +1,13 @@
-links <- read.csv("./Data/links.csv", header = TRUE)
-movies <- read.csv("./Data/movies.csv", header = TRUE)
-ratings <- read.csv("./Data/ratings.csv", header = TRUE)
-tags <- read.csv("./Data/tags.csv", header = TRUE)
+#' Lee los archivos necesarios y devuelve una lista con cada uno de los
+#' ficheros links, movies, ratings y tags
+
+ReadFiles <- function(){
+        links <- read.csv("./Data/links.csv", header = TRUE)
+        movies <- read.csv("./Data/movies.csv", header = TRUE)
+        ratings <- read.csv("./Data/ratings.csv", header = TRUE)
+        tags <- read.csv("./Data/tags.csv", header = TRUE)
+        
+        return(list(Links=links, Movies=movies, Ratings=ratings, Tags=tags))
+}
+
+
