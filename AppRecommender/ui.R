bd55ea5cf15e15fd7bdcbc9f562e2df0dd9a8780
jmcurbelo <jmoyacurbelo@gmail.com>
2019-12-17 16:42:30 -0600

    Update

--- AppRecommender/ui.R
+++ AppRecommender/ui.R
@@ -20,33 +20,52 @@ dashboardPage(skin = "blue",
         tabItems(
             # First tab content
             tabItem(tabName = "Sel_pel",
+                    # fluidRow(
+                    #     
+                    #     box(
+                    #         title = "Seleccione la primera película",
+                    #         background = "blue",
+                    #         selectInput(inputId = "pelicula1", label = "",
+                    #                                     choices = titulos)
+                    #     )
+                    # ),
+                    # 
+                    # fluidRow(
+                    #     box(
+                    #         title = "Seleccione la segunda película",
+                    #         background = "blue",
+                    #         selectInput(inputId = "pelicula2", label = "",
+                    #                                     choices = titulos)
+                    #         
+                    #     )
+                    # ),
+                    # 
+                    # fluidRow(
+                    #     box(
+                    #         title = "Seleccione la tercera película",
+                    #         background = "blue",
+                    #         selectInput(inputId = "pelicula3", label = "",
+                    #                     choices = titulos)
+                    #     )
+                    # )
                     fluidRow(
+                        box(
+                            title = "Seleccione las 5 películas de su preferencia",
+                            background = "blue",
+                            selectInput(inputId = "peliculas", label = "",
+                                        choices = titulos),
+                            actionButton(inputId = "add", label = "Agregar Película", icon = icon("calendar-plus"))
+                        )
+                    ),
+                    fluidRow(
+                        box(
+                            title = "Su selección de películas",
+                            background = "blue",
+                            solidHeader = TRUE,
+                            tableOutput("seleccion"),
+                            actionButton(inputId = "borrar", label = "Borrar selección", icon = icon("backspace"))
+                        )
                         
-                        box(
-                            title = "Seleccione la primera película",
-                            background = "blue",
-                            selectInput(inputId = "pelicula1", label = "",
-                                                        choices = titulos)
-                        )
-                    ),
-                    
-                    fluidRow(
-                        box(
-                            title = "Seleccione la segunda película",
-                            background = "blue",
-                            selectInput(inputId = "pelicula2", label = "",
-                                                        choices = titulos)
-                            
-                        )
-                    ),
-                    
-                    fluidRow(
-                        box(
-                            title = "Seleccione la tercera película",
-                            background = "blue",
-                            selectInput(inputId = "pelicula3", label = "",
-                                        choices = titulos)
-                        )
                     )
             ),
             
